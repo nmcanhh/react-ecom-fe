8b3144cce83a3745a2e4add772a83ebf63295ec8
nmcanhh <nmcanhh@gmail.com>
2021-09-22 19:42:56 +0700

    Tạo Logout với API

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
 import Dashboard from "./components/admin/Dashboard";
 import Profile from "./components/admin/Profile";
 import MasterLayout from "./layouts/admin/MasterLayout";
@@ -13,14 +18,36 @@ axios.defaults.headers.post["Content-Type"] = "application/json";
 axios.defaults.headers.post["Accept"] = "application/json";
 axios.defaults.withCredentials = true;
 
+// Logout
+axios.interceptors.request.use(function (config) {
+  const token = localStorage.getItem("auth_token");
+  config.headers.Authorization = token ? `Bearer ${token}` : "";
+  return config;
+});
+
 function App() {
   return (
     <div className="App">
       <Router>
         <Switch>
           <Route exact path="/" component={Home} />
-          <Route path="/login" component={Login} />
-          <Route path="/register" component={Register} />
+
+          {/* <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} /> */}
+          <Route path="/login">
+            {localStorage.getItem("auth_token") ? (
+              <Redirect to="/" />
+            ) : (
+              <Login />
+            )}
+          </Route>
+          <Route path="/register">
+            {localStorage.getItem("auth_token") ? (
+              <Redirect to="/" />
+            ) : (
+              <Register />
+            )}
+          </Route>
           <Route
             path="/admin"
             name="Admin"
--- src/layouts/frontend/Navbar.js
+++ src/layouts/frontend/Navbar.js
@@ -1,7 +1,53 @@
+import axios from "axios";
 import React from "react";
 import { Link } from "react-router-dom";
+import swal from "sweetalert";
+import { useHistory } from "react-router-dom";
 
 const Navbar = () => {
+  const history = useHistory();
+  const logoutSubmit = (e) => {
+    e.preventDefault();
+    axios.post(`/api/logout`).then((res) => {
+      if (res.data.status === 200) {
+        localStorage.removeItem("auth_token");
+        localStorage.removeItem("auth_name"); // 2 dòng này do được định nghĩa ở trong App.js rồi
+        swal("Success", res.data.message, "success");
+        history.push("/");
+      }
+    });
+  };
+
+  var AuthButtons = "";
+  if (!localStorage.getItem("auth_token")) {
+    AuthButtons = (
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <Link className="nav-link" to="/login">
+            Login
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/register">
+            Register
+          </Link>
+        </li>
+      </ul>
+    );
+  } else {
+    AuthButtons = (
+      <li className="nav-item">
+        <button
+          onClick={logoutSubmit}
+          type="button"
+          className="nav-link btn btn-danger btn-sm text-white"
+        >
+          Logout
+        </button>
+      </li>
+    );
+  }
+
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary shadow stick-top">
       <div className="container">
@@ -31,16 +77,7 @@ const Navbar = () => {
                 Collection
               </Link>
             </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/login">
-                Login
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/register">
-                Register
-              </Link>
-            </li>
+            {AuthButtons}
           </ul>
         </div>
       </div>
